94000e08d06a83da0d8639c1ad4c1af322854a58
Yolanda <limaniyolanda@gmail.com>
2023-04-25 17:08:12 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,7 +12,7 @@
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(43);
 		return (1);
 	} else if (n == 0)
 	    {
@@ -20,7 +20,7 @@
 	       return (0);
 	} else if (n < 0)
 	    {
-		_putchar('-');
+		_putchar(48);
 	}
 	return (-1);
 }
